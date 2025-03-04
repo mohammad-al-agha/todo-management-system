f4c2efa2bf5f478febdd13a0ae0aaa9125f8ac7a
mohammad-al-agha <mohalagha11@gmail.com>
2025-03-04 23:54:10 +0200

    Create <> Login page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use-client";
 
-export default function Home() {
+import Link from "next/link";
+import styles from "./styles/auth.module.css";
+import "./styles/globals.css";
+
+const login = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className={styles.auth_page}>
+      <header className={styles.auth_header}>
+        <h1 className={styles.auth_title}>Sign in to Your Account</h1>
+      </header>
+      <main className={styles.auth_content}>
+        <form className={styles.auth_box}>
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button className="button_blue">Sign in</button>
+        </form>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className={styles.auth_footer}>
+        <p className={styles.auth_footer_text}>
+          Don't have an account? <Link href={"/register"}>Sign up</Link>
+        </p>
       </footer>
     </div>
   );
-}
+};
+export default login;
