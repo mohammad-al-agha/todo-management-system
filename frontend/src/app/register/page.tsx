256a048423403c892fc9104ff22beb9cad3ada5a
mohammad-al-agha <mohalagha11@gmail.com>
2025-03-06 08:09:55 +0200

    Add <> Validations for register page

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -2,19 +2,78 @@
 
 import Link from "next/link";
 import styles from "../styles/auth.module.css";
+import { useState } from "react";
+import { request } from "../utils/request";
+import { localAction } from "../utils/localAction";
+import { useRouter } from "next/navigation";
+import AppInput from "../components/inputs";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import registerSchame from "../utils/register.zod.schema";
 
 const register = () => {
+  const [error, setError] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(registerSchame),
+  });
+
+  const router = useRouter();
+
+  const handleRegister = async (formData: any) => {
+    try {
+      const response = await request({
+        method: "POST",
+        route: "/auth/register",
+        body: formData,
+      });
+
+      localAction("token", response.token);
+
+      router.push("/todos");
+    } catch (error) {
+      console.log(error);
+      setError("Invalid email or password");
+      //Note: Redux?
+    }
+  };
+
   return (
     <div className={styles.auth_page}>
-      <header className={styles.auth_header}>
+      <header className="header_title">
         <h1>Create Your Account</h1>
       </header>
       <main className={styles.auth_content}>
-        <form className={styles.auth_box}>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button className="button_blue">Sign in</button>
+        <form
+          className={styles.auth_box}
+          onSubmit={handleSubmit(handleRegister)}
+        >
+          <AppInput
+            {...register("name")}
+            inputType="text"
+            text="Name"
+            error={errors.name?.message}
+          />
+          <AppInput
+            {...register("email")}
+            inputType="text"
+            text="Email"
+            error={errors.email?.message}
+          />
+          <AppInput
+            {...register("password")}
+            inputType="password"
+            text="Password"
+            error={errors.password?.message}
+          />
+          {error && <p className={styles.auth_error_text}>{error}</p>}
+          <button className="button_blue" type="submit">
+            Register
+          </button>
         </form>
       </main>
       <footer className={styles.auth_footer}>
